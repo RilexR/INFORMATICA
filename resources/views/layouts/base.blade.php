4414eaa9af2c919f208e1b00b27930c3ff14c073
Richard Reyes <richardalexreyes@gmail.com>
2019-04-30 15:00:42 -0400

    navbar fix

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -13,58 +13,29 @@
 	<header>
 		<img class="img-resposnsive" src="{{asset('img/banner.jpg')}}" style="height: 40%; width: 100%">
 	</header>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="">Informatica</a>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar">
+  <a class="navbar-brand" href=""><span class=""></span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#"><b>INFORMATICA</b> <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('login')}}"><b>Login</b></a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="">
-          <b class="caret">Dropdown</b>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('register')}}"><b>Registro</b></a>
         </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-     
-    </form>
   </div>
 </nav>
-<script src="{{ asset('js/bootstrap.min.js')}}"></script>
+@section('content')
 <script src="{{asset('js/jquery.min.js')}}"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script>
-$(function()
-{
-$(".dropdown").hover(
-	function()
-	{
-		$('.dropdown-menu', this).stop(true,true).fadeIn("fast");
-			$(this).toggleClass('open');
-			$('b', this).toggleClass("caret caret-up");
-	},
-	function()
-	{
-		$('.dropdown-menu', this).stop(true,true).fadeOut("fast");
-		$(this).toggleClass('open');
-		$('b', this).toggleClass("caret caret-up");
-	});
-});	
-</script>
+<script src="{{ asset('js/bootstrap.min.js')}}"></script>
 </body>
 </html>
