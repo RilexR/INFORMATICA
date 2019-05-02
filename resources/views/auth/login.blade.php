ebb54c6130f5bb4c20a1eebaf4058d42f8269d09
Richard Reyes <richardalexreyes@gmail.com>
2019-05-02 14:31:26 -0400

    navbar2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,22 @@
 @extends('layouts.base')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('INICIO DE SESION') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electronico') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
+                                @error('Correo electronico')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -26,7 +25,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -45,7 +44,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Recordar sesion') }}
                                     </label>
                                 </div>
                             </div>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,40 +2,40 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>INFORMATICA CORSODEP</title>
 	<link rel="shortcut icon" href="{{ asset('img/favicon.ico')}}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}">
+	<!-- <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css')}}"> -->
+	<script src="{{asset('js/jquery.min.js')}}"></script>
+	<script src="{{ asset('js/bootstrap.min.js')}}"></script>
 	
 </head>
 <body>
 	<header>
 		<img class="img-resposnsive" src="{{asset('img/banner.jpg')}}" style="height: 40%; width: 100%">
 	</header>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar">
-  <a class="navbar-brand" href=""><span class=""></span></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><b>INFORMATICA</b> <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('login')}}"><b>Login</b></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('register')}}"><b>Registro</b></a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-@section('content')
-<script src="{{asset('js/jquery.min.js')}}"></script>
-<script src="{{ asset('js/bootstrap.min.js')}}"></script>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar">
+	  <div class="collapse navbar-collapse">
+	    <ul class="navbar-nav">
+	      <li class="nav-item active">
+	        <a class="nav-link" href=""><b>INFORMATICA</b></a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="{{ route('login')}}"><b>Login</b></a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="{{ route('register')}}"><b>Registro</b></a>
+	        </div>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	@section('content')
+<footer class="footer">
+	<div class="container">
+		<marquee behavior="" direction="">hola</marquee>
+	</div>	
+</footer>
 </body>
 </html>
